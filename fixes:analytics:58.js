b898a87e5e07f5cf021185cffe9aba46b4668d4d
Karl Danninger <kdanninger@gmail.com>
2015-10-21 11:32:42 -0400

    move routes. fix events

--- fixes:analytics:58.js
+++ fixes:analytics:58.js
@@ -2,13 +2,13 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template.someLayout.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.events({
+  Template.someLayout.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
@@ -21,9 +21,3 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
-
-FlowRouter.route("/", {
-  action: function() {
-    BlazeLayout.render(Layout);
-  }
-});
